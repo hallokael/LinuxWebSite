01d3ab7a12880b5d6b793ed6194adc08329fddfa
hallokael <646058898@qq.com>
2017-11-05 23:22:43 +0800

    css

--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ def index(request):
     context={
         'latest_question_list':latest_question_list,
     }
-    return render(request,'index.html',context)
+    return render(request,'polls/index.html',context)
     #output=','.join([q.question_text for q in latest_question_list])
     #return HttpResponse(output)
 def detail(request, question_id):
@@ -21,13 +21,13 @@ def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 def results(request, question_id):
     question=get_object_or_404(Question,pk=question_id)
-    return render(request,'results.html',{'question':question})
+    return render(request,'polls/results.html',{'question':question})
 def vote(request, question_id):
     question=get_object_or_404(Question,pk=question_id)
     try:
         selected_choice=question.choice_set.get(pk=request.POST['choice'])
     except(KeyError,Choice.DoesNotExist):
-        return render(request,'detail.html',{
+        return render(request,'polls/detail.html',{
             'question':question,
             'error_message':"You didn't select a choice.",
         })
